af596c2dbf15bfbea1ec3f00e345a3409b7fac96
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-02-09 16:04:01 -0500

    Time: 20 ms (27.29%), Space: 55.5 MB (76.30%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+        // bruteforce approach, sorting arrays initially, 
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    }
+}
