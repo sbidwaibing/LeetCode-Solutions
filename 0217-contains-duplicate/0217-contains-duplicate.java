345c9f1062678468d72a49de66dc57f9e9eb798f
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-03-08 18:22:44 -0500

    Time: 20 ms (23.61%), Space: 54.9 MB (94.48%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,11 +4,10 @@ class Solution {
         // bruteforce approach, sorting arrays initially, 
         Arrays.sort(nums);
         int n = nums.length;
-        
         for(int i=1;i<n;i++)
         {
             if(nums[i]==nums[i-1]) return true;
-        }
+        }        
         return false;
     }
 }
