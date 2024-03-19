7e47fc9c817f625b7c17e6f392ff77e69425fe85
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-03-19 13:39:25 -0400

    Time: 38 ms (52.89%), Space: 45.9 MB (18.37%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) 
+    {
+        if(hand.length % groupSize != 0) return false; 
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0; i< hand.length; i++)
+        {
+            minHeap.add(hand[i]);
+        }
+        
+        while(!minHeap.isEmpty())
+        {
+            int head = minHeap.poll();
+            for(int i=1; i< groupSize; i++)
+            {
+                if(!minHeap.remove(head+i)) return false;
+            }
+        }
+        
+        return true;
+    }
+}
