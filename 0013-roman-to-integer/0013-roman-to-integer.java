1a42962e7faaa7c45655201a5c9d1782f5388ba4
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-04-06 23:57:14 -0400

    Time: 5 ms (59.38%), Space: 44.8 MB (36.20%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int romanToInt(String s) {
+       
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanValues.get(s.charAt(i));
+
+            if (i < s.length() - 1 && currValue < romanValues.get(s.charAt(i + 1))) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+        }
+
+        return result; 
+    }
+}
