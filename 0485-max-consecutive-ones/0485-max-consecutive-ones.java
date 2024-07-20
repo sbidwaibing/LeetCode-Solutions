11de4db9222f8e72ccff447534facdbe8963ad1d
Sukrut Bidwai <sbidwai@binghamton.edu>
2024-07-19 22:23:01 -0400

    Time: 3 ms (45.47%), Space: 45.7 MB (22.87%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -9,13 +9,10 @@ class Solution {
         {
             if(nums[i] == 1) count++;
             else count = 0;
-            
+    
             max = Math.max(max,count);
-            
             i++;
-            
-        }
-            
+        }  
         return max;
     }
 }
